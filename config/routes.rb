2926f566168f5a878dfd3bf9bf4ed4684d15a573
jdebettencourt <james.s.debettencourt@gmail.com>
2017-03-06 22:22:19 +0000

    delete wrong user model

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   resources :carts
   get 'store/index'
 
-  resources :users
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #root 'products#index'
